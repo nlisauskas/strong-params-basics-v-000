6e5908dd11c5821bdb83d9eb5ed5ba3b61ee4ad4
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-01-15 19:27:59 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(params.require(:post).permit(:title))
+	  @post.update(post_params)
 	  redirect_to post_path(@post)
 	end
 
@@ -27,7 +27,7 @@ class PostsController < ApplicationController
 	  @post = Post.find(params[:id])
 	end
 
-	private 
+	private
 
 	def post_params
 		params.require(:post).permit(:title, :description)
